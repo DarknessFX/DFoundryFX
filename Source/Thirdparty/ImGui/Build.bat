2c76411a68be9a906216a62a47bae10b17726f3b
DarknessFX <35434089+DarknessFX@users.noreply.github.com>
2023-04-06 13:31:56 +0100

    Add files via upload

--- Source/Thirdparty/ImGui/Build.bat
+++ Source/Thirdparty/ImGui/Build.bat
@@ -8,8 +8,8 @@ SET _ResetColor=[0m
 
 ECHO.
 ECHO %_fGreen%%_bBlack%Cleaning cache.%_ResetColor%
-RMDIR /S /Q Binaries
-RMDIR /S /Q Intermediate
+IF EXIST "%CD%\Binaries" ( RMDIR /S /Q "%CD%\Binaries" )
+IF EXIST "%CD%\Intermediate" ( RMDIR /S /Q "%CD%\Intermediate" )
 
 ECHO.
 ECHO %_fGreen%%_bBlack%Copying Implot to ImGui.%_ResetColor%
@@ -27,21 +27,21 @@ cmake --build Intermediate --config Debug
 IF %ERRORLEVEL% NEQ 0 ( GOTO :ERROR )
 
 ECHO.
+ECHO %_fGreen%%_bBlack%Renaming Debug\ImGui.lib to Debug\ImGui_Debug.lib .%_ResetColor%
+COPY /Y Binaries\ImGui.lib Binaries\ImGui_Debug.lib
+COPY /Y Intermediate\CMakeFiles\ImGui.dir\ImGui.pdb Binaries\ImGui_Debug.pdb
+IF %ERRORLEVEL% NEQ 0 ( GOTO :ERROR )
+
+ECHO.
 ECHO %_fGreen%%_bBlack%CMake ImGui Release.%_ResetColor%
 cmake --build Intermediate --config Release
 IF %ERRORLEVEL% NEQ 0 ( GOTO :ERROR )
 
 ECHO.
-ECHO %_fGreen%%_bBlack%Renaming Debug\ImGui.lib to Debug\ImGui_Debug.lib .%_ResetColor%
-COPY /Y Binaries\Debug\ImGui.lib Binaries\Debug\ImGui_Debug.lib
-COPY /Y Binaries\Debug\ImGui.pdb Binaries\Debug\ImGui_Debug.pdb
-IF %ERRORLEVEL% NEQ 0 ( GOTO :ERROR )
-
-ECHO.
 ECHO %_fGreen%%_bBlack%Copying libs to DFoundryFX Plugin Binaries Folder.%_ResetColor%
 IF NOT EXIST "..\..\..\Binaries\Win64\" ( MKDIR "..\..\..\Binaries\Win64" )
-COPY /Y Binaries\Debug\ImGui_Debug*.* ..\..\..\Binaries\Win64\
-COPY /Y Binaries\Release\ImGui*.* ..\..\..\Binaries\Win64\
+COPY /Y Binaries\ImGui_Debug*.* ..\..\..\Binaries\Win64\
+COPY /Y Binaries\ImGui*.* ..\..\..\Binaries\Win64\
 IF %ERRORLEVEL% NEQ 0 ( GOTO :ERROR )
 
 GOTO :END
